0ee9603a2a00fc5d72f8677daae59e9e552a62c3
Kevin Stradtman <stradtkt@gmail.com>
2018-10-15 12:51:39 -0400

    Finished some angular aspects of the app

--- ClientApp/src/app/components/jobs/job-list/job-list.component.ts
+++ ClientApp/src/app/components/jobs/job-list/job-list.component.ts
@@ -1,4 +1,7 @@
+import { JobService } from './../../../services/jobs/job.service';
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-job-list',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-list.component.css']
 })
 export class JobListComponent implements OnInit {
+  public jobList = Jobs[];
+  constructor(public http: Http, private router: Router, private jobService: JobService) {
+    this.jobService.getJobs();
+  }
 
-  constructor() { }
+  getJobs() {
+    this.jobService.getJobs()
+      .subscribe(data => this.jobList = data);
+  }
+  delete(job_id) {
+    const ans = confirm('Do you want to delete this job with the id ' + job_id);
+    if (ans) {
+      this.jobService.deleteJob(job_id)
+        .subscribe((data) => {
+          this.getJobs();
+        }, error => console.error(error));
+    }
+  }
 
   ngOnInit() {
   }
 
+}
+
+interface Jobs {
+  job_id: number;
+  job_title: string;
+  job_desc: string;
+  extra: string;
+  time_length: number;
 }
--- ClientApp/src/app/components/jobs/job-new/job-new.component.ts
+++ ClientApp/src/app/components/jobs/job-new/job-new.component.ts
@@ -1,4 +1,9 @@
+import { JobService } from './../../../services/jobs/job.service';
+import { JobListComponent } from './../job-list/job-list.component';
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-job-new',
@@ -6,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-new.component.css']
 })
 export class JobNewComponent implements OnInit {
-
-  constructor() { }
-
+  jobForm: FormGroup;
+  title:  'Create';
+  job_id: number;
+  errorMessage: any;
+  userList: Array<any> = [];
+  constructor(
+    private fb: FormBuilder,
+    private avRoute: ActivatedRoute,
+    private jobService: JobService,
+    private router: Router
+    ) {
+      if (this.avRoute.snapshot.params['id']) {
+        this.job_id = this.avRoute.snapshot.params['id'];
+      }
+      this.jobForm = this.fb.group({
+        job_id: 0,
+        job_title: ['', Validators.required],
+        job_desc: ['', Validators.required],
+        extra: ['', Validators.required],
+        time_length: ['', Validators.required]
+      });
+    }
   ngOnInit() {
   }
 
